4c778d3bf21767f12d8ea2be8a6c3413b10d0529
MKD <159183602+therealMKD@users.noreply.github.com>
2026-03-28 13:28:55 -0500

    Added a placeholder file for the GUI version, updated gitignore. GUI version coming next weekend

---
+++ GUI Source Code/yt-msd-gui.py
@@ -0,0 +1,4 @@
+#Open Source Software under the Apache License, Version 2.0
+#This is the GUI version of yt-msd. It is the full-featured version of the project, but is larger and *kinda* slower.
+#If you somehow got just this python file, you messed up. Go to https://github.com/therealMKD/yt-msd and find the full exe version.
+#Or, you know, just use this and wing it if you want to
